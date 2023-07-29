e3773b6a82722555bb369cc0d4695245e8f438e8
BangAthar <craft6254@gmail.com>
2023-07-29 20:13:54 +0700

    masalah lagi

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,11 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'public/css/app.css',
+                'public/js/app.js',
             ],
             refresh: true,
         }),
     ],
+    base: 'http://localhost:8000/',
 });
